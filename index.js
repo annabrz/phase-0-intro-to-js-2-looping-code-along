91f805b20aa592ff4ef184732a3b074d601eef28
AnnaAyvazyan <ayvazyananna@yahoo.com>
2022-05-27 14:59:52 -0700

    commit tree 07fc6fe3a8922027172be500244044884dce7c80

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(strArr,event){
+    let newArr = []
+    for(let i=0;i<strArr.length;i++){
+        newArr.push(`Thank you, ${strArr[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArr
+}
+
+function countDown(int){
+while(int>=0){
+    console.log(int--)
+}
+}
